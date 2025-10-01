e470a4cb5ac059950b59696d66f51bc3e85b7408
njse22 <njse22@gmail.com>
2025-10-01 12:03:13 -0500

    semana 9

--- Semana9/BST/src/main/java/structures/BST.java
+++ Semana9/BST/src/main/java/structures/BST.java
@@ -5,7 +5,62 @@ public class BST<T extends Comparable<T>> {
     private Node<T> root;
 
     public void add(T element){
-        //root.getValue().compareTo(element);
+        Node<T> node = new Node<>(element);
+        // Caso Base: el árbol esta vacio
+        if(root == null){
+            root = node;
+        }
+        else {
+            // El valor a agregar es menor que la raiz
+            if(root.getLeft() == null && root.getValue().compareTo(element) > 0){
+                root.setLeft(node);
+            }
+            else if(root.getLeft() != null && root.getValue().compareTo(element) > 0){
+                // llamado al método privado
+                add(root.getLeft(), node);
+            }
+            // El valor a agregar es mayor que la raíz
+            else if(root.getRight() == null && root.getValue().compareTo(element) < 0){
+                root.setRight(node);
+            }
+            else if(root.getRight() != null && root.getValue().compareTo(element) < 0){
+                add(root.getRight(), node);
+            }
+            // si es igual
+            else {
+                // No se agrega
+                // EXception (Por ejemplo)
+            }
+        }
+    }
+
+    private void add(Node<T> current, Node<T> node){
+        // El valor del Node es menor que el valor del nodo
+        // actual, se agrega a la izquierda
+        if (current.getValue().compareTo(node.getValue()) > 0){
+            // Caso Base: la izquierda es Null
+            if(current.getLeft() == null){
+                current.setLeft(node);
+            }
+            // Caso Recursivo: Se debe seguir buscando
+            else {
+               add(current.getLeft(), node);
+            }
+        }
+        // El valor del Node es mayor que el valor del nodo actual (current)
+        // se agrega a la derecha del current
+        else if(current.getValue().compareTo(node.getValue()) < 0){
+            if(current.getRight() == null){
+                current.setRight(node);
+            }
+            else {
+                add(current.getRight(), node);
+            }
+        }
+        else {
+            // No agregamos el nodo
+        }
+
     }
 
     public boolean search(T element){
@@ -17,7 +72,24 @@ public class BST<T extends Comparable<T>> {
     }
 
     public String inOrder(){
-        return "";
+        String msj = "";
+
+        if (root != null){
+            msj = inOrder(root);
+        }
+        else {
+            msj = "Is empty tree";
+        }
+        return msj;
+    }
+
+    private String inOrder(Node<T> curent){
+        if(curent != null){
+            return inOrder(curent.getLeft()) + " " + curent.getValue() + " " + inOrder(curent.getRight());
+        }
+        else {
+            return "";
+        }
     }
 
 }
---
+++ Semana9/BST/src/main/java/ui/Main.java
@@ -0,0 +1,16 @@
+package ui;
+
+import structures.BST;
+
+public class Main {
+    public static void main(String[] args) {
+        BST<Integer> binaryTreeIntegers = new BST<>();
+        binaryTreeIntegers.add(50);
+        binaryTreeIntegers.add(60);
+        binaryTreeIntegers.add(40);
+        binaryTreeIntegers.add(45);
+        binaryTreeIntegers.add(41);
+
+        System.out.println(binaryTreeIntegers.inOrder());
+    }
+}
